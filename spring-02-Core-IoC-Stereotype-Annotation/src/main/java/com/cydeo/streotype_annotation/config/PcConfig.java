751f28bfe027e5ab3cc0e2f58bf2859fd4446687
Igariok <igariokpojar@yahoo.com>
2022-09-12 11:43:51 -0500

    Stereotype Annotation

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -0,0 +1,10 @@
+package com.cydeo.streotype_annotation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.cydeo") // scan start from cydeo package or com.cydeo.stereotype_annotation.config
+public class PcConfig {
+}
