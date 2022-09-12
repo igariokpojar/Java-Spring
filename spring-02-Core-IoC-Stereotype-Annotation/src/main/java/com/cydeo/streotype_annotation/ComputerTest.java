9db4e0cef034dd027fbb175c4d754aebd7143874
Igariok <igariokpojar@yahoo.com>
2022-09-12 11:53:18 -0500

    Stereotype Annotation

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
@@ -1,6 +1,7 @@
 package com.cydeo.streotype_annotation;
 
 import com.cydeo.streotype_annotation.config.PcConfig;
+import com.cydeo.streotype_annotation.monitorfactory.Monitor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,6 +9,10 @@ public class ComputerTest {
 
     public static void main(String[] args) {
 
-        ApplicationContext container = new AnnotationConfigApplicationContext(PcConfig.class);
+        ApplicationContext container = new AnnotationConfigApplicationContext(PcConfig.class); // create a container and add PcConf class
+
+        Monitor theMonitor = container.getBean(Monitor.class);
+        System.out.println(theMonitor.getSize());
+
     }
 }
