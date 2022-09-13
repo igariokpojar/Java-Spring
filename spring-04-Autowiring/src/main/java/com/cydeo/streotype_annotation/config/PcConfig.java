e06fb0229b4266642fbf4adcecf9601779aa9637
Igariok <igariokpojar@yahoo.com>
2022-09-13 08:46:53 -0500

    Autowired

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -3,9 +3,9 @@ package com.cydeo.streotype_annotation.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
-@ComponentScan("com.cydeo") // scan start from cydeo package or com.cydeo.stereotype_annotation.config
+@ComponentScan("com.cydeo") // go to each package and look for the classes with is Annotated whit @Component and ==>
+//create a Bean and put on Container and manage it for me
 
 public class PcConfig {
 
