956c440e0e6a6c6559e8dda6a139c7b1c3b0e7cb
Igariok <igariokpojar@yahoo.com>
2022-09-13 11:59:09 -0500

    Abstraction

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,28 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.proxy.EmailCommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import com.cydeo.repository.DBCommentRepository;
+
+public class CommentService {
+
+    private CommentRepository commentRepository; // for loosing couple we use Interface, implementation can change
+
+    private CommentNotificationProxy commentNotificationProxy;// for loosing couple we use Interface
+
+    public void publishComment(Comment comment){
+
+        // save in the DB
+        commentRepository.storeComment(comment);
+
+
+        // Send email
+        commentNotificationProxy.sendComment(comment);
+
+
+    }
+
+}
+
