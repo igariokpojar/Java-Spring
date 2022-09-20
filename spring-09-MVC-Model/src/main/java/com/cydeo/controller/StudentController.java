49a9a527fc47196f12b098c05a34f19d32d4def2
Igariok <igariokpojar@yahoo.com>
2022-09-20 11:02:25 -0500

    MVC Model

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,17 +1,42 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
 
     @RequestMapping("/welcome")
+    //Model is Interface tha is carrying data to view
     public String homePage(Model model){
 
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
+
+        String subject = "Spring Boot";
+        model.addAttribute("subject",subject);
+
+        int studentId = new Random().nextInt();
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        model.addAttribute("numbers",numbers);
+
+        LocalDate dt = LocalDate.now();
+        model.addAttribute("date",dt);
+
+        Student student = new Student(1,"Igariok","Pojar");
+        model.addAttribute("student",student);
 
         return "student/welcome";
     }
