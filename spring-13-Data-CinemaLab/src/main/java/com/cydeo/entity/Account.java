33baa7cb3921f6dbe2a264f5151aebcf62db10a9
Igariok <igariokpojar@yahoo.com>
2022-10-25 15:16:29 -0500

    Cinema Lab

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Account.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Account.java
@@ -22,6 +22,6 @@ public class Account extends BaseEntity{
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
-    @OneToOne(mappedBy = "account")
+    @OneToOne(mappedBy = "account") // to drop  foreign key on Account table
     private User user;
 }
