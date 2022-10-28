7053e6741d94008f5c0be25926412925f479fae5
Igariok <igariokpojar@yahoo.com>
2022-10-28 10:38:36 -0500

    Spring DATA Queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/entity/Employee.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Employee.java
@@ -3,6 +3,7 @@ package com.cydeo.entity;
 import com.cydeo.enums.Gender;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLInsert;
 
 import javax.persistence.*;
 import java.time.LocalDate;
