6111da93eb23429ddd9ca702713efb0f2180f2e2
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-11-29 13:10:30 -0600

    Controller methods

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -9,9 +9,7 @@ import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -57,5 +55,13 @@ public class SchoolController {
         return ResponseEntity.ok(new ResponseWrapper("Address has successfully retrieved",addressDTO));
 
     }
+
+    @PutMapping("/address/{id}")
+    public AddressDTO updateAddress(@PathVariable("id")Long id, @RequestBody AddressDTO addressDTO) throws Exception {
+        addressDTO.setId(id);
+       return addressService.update(addressDTO);
+    }
+
+
 
 }
