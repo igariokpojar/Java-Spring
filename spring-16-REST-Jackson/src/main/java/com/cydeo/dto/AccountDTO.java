5bea36e1d74ebdf20b66246d5ca6cc49f1da08d1
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-11-29 10:36:17 -0600

    Spring Rest

--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -1,12 +1,18 @@
 package com.cydeo.dto;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@JsonIgnoreProperties(value = {"address","country","state"},ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AccountDTO {
 
+    @JsonIgnore
     private String name;
     private String address;
     private String country;
@@ -14,6 +20,9 @@ public class AccountDTO {
     private String city;
     private Integer age;
     private String postalCode;
+
+    @JsonBackReference // This field is not going to be  serialized(we are not going to see the Object to be converted to Json account
     private UserDTO user;
+
 
 }
--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
@@ -1,17 +1,26 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true) // preventing the User to send the unknown property
 public class UserDTO {
 
     private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // some field of json property
     private String password;
+
     private String username;
     private UserRole role;
+
+    @JsonManagedReference // // This field is going to be serialized(we are going to see the Object to be converted to Json account
     private AccountDTO account;
 
 }
